6f242e0c1fe4aaa1632a91365e63ec2841ab91ba
moblaze360 <victorvalenti@yahoo.co.uk>
2021-01-08 15:35:28 +0200

    Updates

---
+++ precache-manifest.4c0abd1d27c01754ae57fc6fb03458f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618667649ede09694171a287b3f84ac4",
+    "url": "/faces/index.html"
+  },
+  {
+    "revision": "9ad041c74daecf2f9633",
+    "url": "/faces/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2d0d0e8f2c6a640e3061",
+    "url": "/faces/static/css/main.f14162dc.chunk.css"
+  },
+  {
+    "revision": "9ad041c74daecf2f9633",
+    "url": "/faces/static/js/2.b53e66b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faces/static/js/2.b53e66b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0d0e8f2c6a640e3061",
+    "url": "/faces/static/js/main.5a1512a0.chunk.js"
+  },
+  {
+    "revision": "352e26b6cf1a43e85bfa",
+    "url": "/faces/static/js/runtime-main.f726568d.js"
+  },
+  {
+    "revision": "195d0634b97cc5727e78d2f81f80067d",
+    "url": "/faces/static/media/Money-mu.195d0634.svg"
+  }
+]);
